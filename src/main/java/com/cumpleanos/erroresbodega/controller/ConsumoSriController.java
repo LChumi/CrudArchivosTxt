96503165b86474475645acefaf25e243b8457388
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-09-24 12:24:38 -0500

    feat: added resilience to added timeout into httpRequest

--- src/main/java/com/cumpleanos/erroresbodega/controller/ConsumoSriController.java
+++ src/main/java/com/cumpleanos/erroresbodega/controller/ConsumoSriController.java
@@ -12,8 +12,7 @@ import com.cumpleanos.erroresbodega.models.Cliente;
 import com.cumpleanos.erroresbodega.models.api.ContribuyenteSri;
 import com.cumpleanos.erroresbodega.models.api.PersonaSri;
 import com.cumpleanos.erroresbodega.services.ClienteService;
-import com.cumpleanos.erroresbodega.services.http.SriClientV1Impl;
-import com.cumpleanos.erroresbodega.services.http.SriClientV2Impl;
+import com.cumpleanos.erroresbodega.services.SriServiceWrapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,25 +26,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ConsumoSriController {
 
-    private final SriClientV1Impl service1;
-    private final SriClientV2Impl service2;
-    private final ClienteService  clienteService;
+    private final ClienteService clienteService;
+    private final SriServiceWrapper sriServiceWrapper;
 
     @GetMapping(value = "/ced-ruc/{ced}/{ident}")
     public String getCedRuc(@PathVariable String ced, @PathVariable String ident) {
         ced = ced.trim();
         ident = ident.trim().toUpperCase();
-        try {
-            PersonaSri p = service2.getClient(ced, ident);
 
+        try {
+            PersonaSri p = sriServiceWrapper.getPersona(ced, ident).get();
             if (p == null || p.getNombreCompleto() == null || p.getNombreCompleto().isEmpty()) {
-                ContribuyenteSri c = service1.getClient(ced);
+                ContribuyenteSri c = sriServiceWrapper.getContribuyente(ced).get();
                 if (c == null || c.getContribuyente() == null || c.getContribuyente().getNombreComercial() == null) {
                     return "";
                 }
-                return c.getContribuyente().getNombreComercial()+"|"+c.getContribuyente().getIdentificacion();
+                return c.getContribuyente().getNombreComercial() + "|" + c.getContribuyente().getIdentificacion();
             } else {
-                return p.getNombreCompleto()+"|"+p.getIdentificacion();
+                return p.getNombreCompleto() + "|" + p.getIdentificacion();
             }
         } catch (NullPointerException e) {
             return "Error: Valores nulos inesperados.";
@@ -60,10 +58,10 @@ public class ConsumoSriController {
         ced = ced.trim();
         ident = ident.trim().toUpperCase();
         try {
-            PersonaSri p = service2.getClient(ced, ident);
+            PersonaSri p = sriServiceWrapper.getPersona(ced, ident).get();
 
             if (p == null || p.getNombreCompleto() == null || p.getNombreCompleto().isEmpty()) {
-                ContribuyenteSri c = service1.getClient(ced);
+                ContribuyenteSri c = sriServiceWrapper.getContribuyente(ced).get();
                 if (c == null || c.getContribuyente() == null || c.getContribuyente().getNombreComercial() == null) {
                     Cliente cliente = clienteService.buscarPorCedula(ced);
                     return ResponseEntity.ok((cliente == null) ? "" : cliente.getCliNombre());
@@ -75,7 +73,7 @@ public class ConsumoSriController {
         } catch (Exception e) {
             // Log del error para facilitar su rastreo
             log.error("Error inesperado: ", e);
-            return ResponseEntity.badRequest().body("Error inesperado");
+            return ResponseEntity.noContent().build();
         }
     }
 
---
+++ src/main/java/com/cumpleanos/erroresbodega/services/SriServiceWrapper.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2025 Luis Chumi.
+ * Este software está licenciado bajo la Licencia Pública General de GNU versión 3. Puedes encontrar una copia de la licencia en https://www.gnu.org/licenses/gpl-3.0.html.
+ *
+ * Para consultas o comentarios, puedes contactarme en "luischumi.9@gmail.com".
+ * Me gustaría ser reconocido por mi trabajo y estar abierto a colaboraciones o enseñanzas sobre el programa.
+ */
+
+package com.cumpleanos.erroresbodega.services;
+
+import com.cumpleanos.erroresbodega.http.ClientSriV1;
+import com.cumpleanos.erroresbodega.http.ClientSriV2;
+import com.cumpleanos.erroresbodega.models.api.ContribuyenteSri;
+import com.cumpleanos.erroresbodega.models.api.PersonaSri;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+public class SriServiceWrapper {
+
+    private final ClientSriV1 clientSriV1;
+    private final ClientSriV2 clientSriV2;
+
+    @TimeLimiter(name = "sriTimeout", fallbackMethod = "falbackPersona")
+    public CompletableFuture<PersonaSri> getPersona(String ced, String ident){
+        return CompletableFuture.supplyAsync(() ->
+                clientSriV2.consultar(ced, ident).getBody()
+        );
+    }
+
+    @TimeLimiter(name = "sriTimeout", fallbackMethod = "falbackContribuyente")
+    public CompletableFuture<ContribuyenteSri> getContribuyente(String ced){
+        return CompletableFuture.supplyAsync(() ->
+                clientSriV1.consultar(ced).getBody()
+        );
+    }
+
+    private CompletableFuture<PersonaSri> fallbackPersona(String ced, String ident, Throwable ex) {
+        return CompletableFuture.completedFuture(null);
+    }
+
+    private CompletableFuture<ContribuyenteSri> fallbackContribuyente(String ced, Throwable ex) {
+        return CompletableFuture.completedFuture(null);
+    }
+
+}
